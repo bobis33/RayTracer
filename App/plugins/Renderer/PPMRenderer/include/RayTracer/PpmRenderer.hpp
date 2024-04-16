628a41eeba76a1d6eecb297f405d9c1b80deca55
MASINA Elliot <elliot.masina@epitech.eu>
2024-04-16 10:10:22 +0200

    [FEAT]: init ppm renderer

---
+++ App/plugins/Renderer/PPMRenderer/include/RayTracer/PpmRenderer.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** Raytracer | renderer
+** File description:
+** PpmRenderer.hpp
+*/
+
+#ifndef RAYTRACER_PPM_RENDERER_HPP
+#define RAYTRACER_PPM_RENDERER_HPP
+
+#include "RayTracer/Abstraction/IRenderer.hpp"
+
+namespace RayTracer {
+
+    class PpmRenderer : IRenderer {
+
+        public:
+            ~PpmRenderer() override = default;
+
+    }; // PpmRenderer
+
+} // namespace RayTracer
+
+#endif // RAYTRACER_PPM_RENDERER_HPP
---
+++ App/plugins/Renderer/PPMRenderer/src/ppmRenderer.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2024
+** Raytracer | renderer
+** File description:
+** renderer.cpp
+*/
+
+#include "RayTracer/PpmRenderer.hpp"
